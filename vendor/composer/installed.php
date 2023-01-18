44b97549b066fd30420821fd61b548083b035e84
josephosismar <josephsismart@gmail.com>
2023-01-18 22:29:12 +0800

    x
    x

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'aad275083413590cd944f0df6f5e970965d80a8c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'aad275083413590cd944f0df6f5e970965d80a8c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
